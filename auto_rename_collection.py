e01c587d2957e80fcc7f232ffe14625b1b8bd422
zhangguisen <zhangguisen@pcidata.cn>
2019-06-25 16:17:52 +0800

    增加是否保存源文件

--- auto_rename_collection.py
+++ auto_rename_collection.py
@@ -1,20 +1,26 @@
 import os
 import time
+import shutil
 
 
 # 功能点
 # 1. 输入指定的文件路径
-# 2. 展示路径下的文件详情
+# 2. 展示路径下的文件详情, 如果没有文件就退出
 # 3. 输入指定的输出路径（如果不指定，就在指定的路径默认建一个output文件夹）
-# 4. 简单的替换规则，如指定替换的字符串的 开始位置和结束位置，指定序列号开始数字等
-# 5. 日志记录：输出txt文件，记录源文件 --> 新文件 的对应关系。
-# 6. 尝试对正则替换的支持
+# 4. 简单的替换规则：
+#   4.1 指定替换的字符串
+#   4.2 指定替换的开始位置，
+#   4.3 指定序列号开始数字，用于拼接在重命名的文件名末尾
+# 5. 在指定文件路径下，建一个日志文件夹，日志记录：输出txt文件，记录源文件 --> 新文件 的对应关系。
+# 6. 增加一个选项问用户是否需要保存原来的文件
+# 7. 尝试对正则替换的支持 TODO
 
 # 日志记录功能
-def gen_my_log(msg,path):
+def gen_my_log(msg, path):
     # file = r'C:\Users\zhanggs\Desktop\test\log-remane20180830.txt'
     log_path = path + '\\log\\'
-    make_dir(log_path)
+    if not (os.path.exists(log_path)):
+        make_dir(log_path)
     file = log_path + "log" + time.strftime("%Y%m%d%H%M%S", time.localtime()) + ".txt"
     with open(file, 'a+', encoding='utf-8') as f:
         f.write(msg)
@@ -27,9 +33,9 @@ def user_input_dir():
             if os.path.exists(input_dir):
                 return input_dir
             else:
-                print("请输入需要批量重命名文件所在的路径（请用斜杠/）:")
+                print("x  请输入需要批量重命名文件所在的路径（请用斜杠/）:")
         except Exception:
-            print("请输入需要批量重命名文件所在的路径（请用斜杠/）:")
+            print("x  请输入需要批量重命名文件所在的路径（请用斜杠/）:")
 
 
 def user_output_dir():
@@ -39,12 +45,12 @@ def user_output_dir():
             if os.path.exists(output_dir):
                 return output_dir
             else:
-                print("路径不存在，创建中……")
+                print("###  路径不存在，创建中……")
                 make_dir(output_dir)
-                print("路径创建成功")
+                print("###  路径创建成功")
                 return output_dir
         except Exception:
-            print("重命名完成后文件保存的路径（请用斜杠/）:")
+            print("x  请输入文件路径（请用斜杠/）:")
 
 
 def user_yes_or_no():
@@ -58,9 +64,9 @@ def user_yes_or_no():
             elif user_yes_no.upper() in no_words:
                 return "N"
             else:
-                print("请输入：Y 或者 N")
+                print("x  请输入：Y 或者 N")
         except Exception:
-            print("请输入：Y 或者 N")
+            print("x  请输入：Y 或者 N")
 
 
 def user_input_notnull():
@@ -70,23 +76,23 @@ def user_input_notnull():
             if input_str.strip() != '':
                 return input_str
             else:
-                print("输入不能为空")
+                print("x  输入不能为空")
         except Exception:
-            print("输入不能为空")
+            print("x  输入不能为空")
 
 
 def make_dir(t_path):
     folder = os.path.exists(t_path)
     if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
         os.makedirs(t_path)  # makedirs 创建文件时如果路径不存在会创建这个路径
-        print("---  创建新文件 ---" + t_path)
-        print("---  OK  ---")
+        print("###  创建新文件:" + t_path)
+        print("###  OK")
     else:
-        print("---  路径已存在，不用新建  ---")
+        print("###  路径已存在，不用新建: "+ t_path)
 
 
 def show_files_in_dir(files_dir):
-    print("---  路径下的文件如下：  ---")
+    print("###   路径下的文件如下：")
     temp_index = 0
     file_list = os.listdir(files_dir)
     for files in file_list:  # 遍历所有文件
@@ -98,8 +104,8 @@ def show_files_in_dir(files_dir):
             print(show_dir)
     if temp_index == 0:
         # print("--没有文件，退出程序--")
-        exit("--没有文件，退出程序--")
-    print("------------")
+        exit("###  没有文件，退出程序")
+    print("###  ------------")
 
 
 def user_num_input():
@@ -109,19 +115,18 @@ def user_num_input():
             if 0 < user_num < 1000:
                 return user_num
             else:
-                print("请输入1000以内的整数数字: ")
+                print("x  请输入1000以内的整数数字: ")
         except Exception:
-            print("请输入1000以内的整数数字: ")
+            print("x  请输入1000以内的整数数字: ")
 
 
-def rename(new_str, old_path, new_path, start_index=2, start_seq=1):
-    print("重命名进行中: ")
+def rename(new_str, old_path, new_path, start_index=1, start_seq=1, save_old_file=1):
+    print("###  重命名进行中: ")
     if start_index <= 1:
         start_index = 1
-    # path = r"C:\Users\zhanggs\Desktop\test\input"
-    # newpath = r"C:\Users\zhanggs\Desktop\test\newFile"
+
     file_list = os.listdir(old_path)  # 该文件夹下所有的文件（包括文件夹）
-    # i = 501  # 由于有文件尾号一样，所以从大一点的开始
+
     i = start_seq
     for files in file_list:  # 遍历所有文件
 
@@ -131,13 +136,15 @@ def rename(new_str, old_path, new_path, start_index=2, start_seq=1):
         filename = os.path.splitext(files)[0]  # 文件名
         new_name1 = filename[:start_index-1]
         new_name2 = new_str
-        # if filename[8:16] == new_str: # 如果是指定的日期就跳过该文件
-        #     continue
         new_name3 = gen_end_index(str(i))
         file_type = os.path.splitext(files)[1]  # 文件扩展名
         new_name = new_name1 + new_name2 + new_name3
         new_dir = os.path.join(new_path, new_name + file_type)
-        os.rename(old_dir, new_dir)  # 重命名
+        if save_old_file == 1:
+            # 保留源文件
+            shutil.copyfile(old_dir, new_dir)  # 复制重命名
+        else:
+            os.rename(old_dir, new_dir)  # 重命名
         i += 1
         msg = old_dir + '重命名完成：' + new_dir
         print(msg)
@@ -150,30 +157,37 @@ def gen_end_index(index):
 
 
 if __name__ == '__main__':
-    print("请输入需要批量重命名文件所在的路径: ")
+    print("\n>>>  请输入需要重命名的文件所在的路径: ")
     path = user_input_dir()
     show_files_in_dir(path)  # 展示路径下的文件详情
-    print("是否需要指定文件保存的路径, Y/N ")
+    print("\n>>>  是否需要保留源文件, Y/N ")
+    save_flag = user_yes_or_no()
+    if save_flag == 'Y':
+        is_save = 1
+    else:
+        is_save = 0
+    print("\n>>>  是否需要指定重命名后的文件保存的路径, Y/N ")
     flag = user_yes_or_no()
     if flag == 'Y':
         new_path = user_output_dir()
     else:
         new_path = path + '/output/'
         make_dir(new_path)
-    print("请输入需要替换成的字符串: ")
+    print("\n>>>  请输入文件名需要替换成的字符串: ")
     replace_str = user_input_notnull()
-    print("是否需要指定文件序列号开始数字, Y/N ")
+    print("\n>>>  是否需要指定文件后缀的序列号开始数字（默认从1开始）, Y/N ")
     num_flag = user_yes_or_no()
     if num_flag == 'Y':
-        print("请输入整数： ")
+        print("x  请输入整数： ")
         start_seq = user_num_input()
     else:
         start_seq = 1
-    print("是否需要指定从源文件名第几位开始替换, Y/N ")
+    print("\n>>>  是否需要指定从源文件名的第几位开始替换（默认从开始位置进行替换）, Y/N ")
     index_flag = user_yes_or_no()
     if index_flag == 'Y':
-        print("请输入整数： ")
+        print("x  请输入整数： ")
         start_i = user_num_input() + 1
     else:
         start_i = 0
-    rename(replace_str, path, new_path, start_i, start_seq)
+    rename(replace_str, path, new_path, start_i, start_seq, is_save)
+    print("\n>>>  重命名后的文件保存在："+new_path)
